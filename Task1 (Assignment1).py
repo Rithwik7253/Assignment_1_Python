c2e61045e206d3132f73406073600be6609300d9
Rithwik7253 <97350484+Rithwik7253@users.noreply.github.com>
2025-06-07 21:33:12 +0530

    Add files via upload

---
+++ Task1 (Assignment1).py
@@ -0,0 +1,7 @@
+a = float(input('Enter a number'))
+b = float(input('Enter another number'))
+
+print('The addition of the numbers is ', a+b)
+print('The subtraction n of the numbers is ', a-b)
+print('The multiplication of the numbers is ', a*b)
+print('The division of the numbers is ', a/b)
---
+++ Task2 (Assignment 1).py
@@ -0,0 +1,4 @@
+fn = input('Enter your first name')
+ln = input('Enter your last name')
+
+print('Hello',fn,ln,', welcome to the python program. Enjoy your experience here!')
